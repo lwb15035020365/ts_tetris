76fa4faffdafd71acfcdac3abdf75ded02b1c31f
Lwb <1679520105@qq.com>
2023-03-03 23:00:15 +0800

    reset init & start

--- index.js
+++
@@ -1,5 +0,0 @@
-const path = require('path')
-
-console.log(path.resolve('b', 'c'), __dirname);
-console.log('git')
-console.log('git1');
--- test copy.js
+++
@@ -1,3 +0,0 @@
-console.log('test');
-console.log('test1');
-console.log('test2');
