b530569f44c26e845d0e10f2a8e25f1e67b22249
Lwb <1679520105@qq.com>
2023-03-05 20:37:22 +0800

    创建小方块类

---
+++ src/core/Square.ts
@@ -0,0 +1,41 @@
+import { IViewer, Point } from "./types";
+
+export class Square {
+
+  private _point: Point = {
+    x: 0,
+    y: 0
+  }
+
+  private _color: string = ""
+
+  private _viewer?: IViewer;
+
+  public get viewer() {
+    return this._viewer
+  }
+
+  public set viewer(v) {
+    this._viewer = v;
+  }
+
+  public get point() {
+    return this._point;
+  }
+
+  public set point(val) {
+    this._point = val;
+    if (this._viewer) {
+      this._viewer.show();
+    }
+  }
+
+  public get color() {
+    return this._color
+  }
+
+  public set color(val) {
+    this._color = val;
+  }
+
+}
