f34def956f7e632abfac28aefc78efebc806b069
Lwb <1679520105@qq.com>
2023-03-05 20:37:22 +0800

    创建小方块类

---
+++ src/core/types.ts
@@ -0,0 +1,12 @@
+export interface Point {
+  readonly x: number;
+  readonly y: number
+}
+
+export interface IViewer {
+  //显示
+  show(): void;
+
+  //移除
+  remove(): void;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,23 @@
-let str: string = 'hello,world';
-console.log(str);
+import { Square } from "./core/Square";
+import { IViewer } from "./core/types";
 
-export { };
+class SquareConsoleViewer implements IViewer {
+
+  constructor(private square: Square) { }
+
+  show(): void {
+    console.log(this.square.point, this.square.color);
+  }
+
+  remove(): void {
+
+  }
+
+}
+
+const sq = new Square();
+sq.viewer = new SquareConsoleViewer(sq);
+sq.point = {
+  x: 5,
+  y: 6
+}
